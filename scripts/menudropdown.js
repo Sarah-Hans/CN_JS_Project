6a1e3d339ca6812ec1e848c5e6a4b3a97aefd036
Sarah-Hans <leawine@gmail.com>
2021-03-30 15:09:24 +0200

    feat : formulaire d'ajout d'article fixes #19

--- scripts/menudropdown.js
+++ scripts/menudropdown.js
@@ -1,10 +1,10 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+/* Quand l'utilisateur clique sur le bouton dropdown, 
+inversion entre hiding et showing le dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown menu if the user clicks outside of it
+  // ferme le sous menu si l'utilisateur clique dessus
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
