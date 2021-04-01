d84a41043923558fd11b3a80ef7bc97e994d5e8c
Sarah-Hans <leawine@gmail.com>
2021-04-01 11:10:26 +0200

    feat : effacer un article - affichage bouton et fonction qui efface l'article fixes #24

--- scripts/sendpost.js
+++ scripts/sendpost.js
@@ -7,15 +7,36 @@ $(document).ready(function(){
             titre: document.getElementById('titre_article').value,
             texte: document.getElementById('text_article').value,
             afficherPost: function() {
-                return '<div class="one-post"><h3>' + this.titre + '</h3><p class="author">Rédigé par ' + this.auteur + '</p><div class="post-content">' + this.texte + '</div>';
+                return '<div class="one-post"><h3>' + this.titre + '</h3><p class="author">Rédigé par ' + this.auteur + '</p><div class="post-content"><p>' + this.texte + '</p></div><button class="bouton__suppr" title="supprimer">Supprimer</button>';
             }
         };
         console.log(Article);
         event.preventDefault();
 
+
         $('.articles-cat').append(function() {
             return Article.afficherPost();
         });
 
+     /**
+    * Suppression d'un article
+    */
+
+      let container = document.querySelector('.articles-cat')
+      let post = document.querySelector(".one-post")
+      let bouton_suppr = document.querySelector('.bouton__suppr')
+      if (post !== null) {
+          bouton_suppr.addEventListener('click', function(e) {
+              e.preventDefault();
+              console.log(container)
+              container.removeChild(post)
+          })
+      }
+
     });
+
+   
+ 
+
+    
 })
